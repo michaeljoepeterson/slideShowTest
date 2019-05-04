ab663dbbf05788b6f890472896869fc25157d51d
Michael Peterson <michaeljoepeterson@gmail.com>
2019-05-04 14:38:44 -0600

    tested out different image sizing

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,8 +1,16 @@
-function SlideShow(arrowLeft,arrowRight){
+function SlideShow(arrowLeft,arrowRight,slideShowContainerID){
 	this.leftArrow = document.getElementById(arrowLeft);
 	this.rightArrow = document.getElementById(arrowRight);
+	this.slideShowContainer = document.getElementById(slideShowContainerID);
+	this.slideShowWidth = this.slideShowContainer.scrollWidth;
+	this.slideShowHeight = this.slideShowContainer.scrollHeight;
+	console.log(this.slideShowWidth);
 	this.initEventListeners();
 }
+
+//if keep 1000px width then can do a resize calc/init calc
+//max-width - center div container width / 2 = margin left
+//could do that for all sizes? resize event on a timer?
 
 SlideShow.prototype.initEventListeners = function() {
 	this.leftArrow.addEventListener("click",function(e){
@@ -23,7 +31,7 @@ SlideShow.prototype.rightArrowClicked = function(event) {
 };
 
 function initSlideShow(){
-	var slideshow = new SlideShow("arrowLeft","arrowRight");
+	var slideshow = new SlideShow("arrowLeft","arrowRight","slideShowContainer1");
 }
 
 window.onload = initSlideShow;
